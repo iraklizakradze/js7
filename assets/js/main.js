6911ccf43e1ba1c99f520d1a9f8d0ae6a6ee04c1
iraklizakradze <iraklizakradze@gmail.com>
2024-02-19 16:47:50 +0400

    js7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,80 @@
 // ----- HOMEWORK 7 ----- //
-const apiUsers = document.getElementById('api-users')
+// const apiUsers = document.getElementById('api-users')
 
-function getUsers() {
+// function getUsers() {
 
-    const request = new XMLHttpRequest();
+//     const request = new XMLHttpRequest();
 
-    request.open('GET', 'https://reqres.in/api/unknown');
-    request.addEventListener('load', function() {
-        // console.log(this.responseText);
-        const mosuliInfo = this.responseText;
-        const mosuliInfoJS = JSON.parse(mosuliInfo);
-        console.log(mosuliInfoJS);
+//     request.open('GET', 'https://reqres.in/api/unknown');
+//     request.addEventListener('load', function() {
+//         // console.log(this.responseText);
+//         const mosuliInfo = this.responseText;
+//         const mosuliInfoJS = JSON.parse(mosuliInfo);
+//         console.log(mosuliInfoJS);
 
-        const ulElement = document.createElement('ul');
+//         const ulElement = document.createElement('ul');
 
-        mosuliInfoJS.data.forEach((element) => {
-            // console.log(element)
-            const li = document.createElement('li');
+//         mosuliInfoJS.data.forEach((element) => {
+//             // console.log(element)
+//             const li = document.createElement('li');
 
-            // li.textContent = element.name + ' ' + element.color;
-            li.textContent = `${element.name} ${element.color}`;
-            ulElement.appendChild(li);
-        });
+//             // li.textContent = element.name + ' ' + element.color;
+//             li.textContent = `${element.name} ${element.color}`;
+//             ulElement.appendChild(li);
+//         });
 
-        apiUsers.appendChild(ulElement)
+//         apiUsers.appendChild(ulElement)
 
-    })
+//     })
 
-    request.addEventListener('error', function() {
-        const pDesc = document.createElement('p');
-        pDesc.textContent = 'Server Error';
+//     request.addEventListener('error', function() {
+//         const pDesc = document.createElement('p');
+//         pDesc.textContent = 'Server Error';
         
-        apiUsers.appendChild(pDesc);
-    })
-    request.send();
-}
-getUsers();
+//         apiUsers.appendChild(pDesc);
+//     })
+//     request.send();
+// }
+// getUsers();
 
 
 //////////////////////////////////////////////////////////////////////////
+
+
+// fetch
+const apiUsers = document.getElementById('api-users')
+
+
+fetch('http/reqres.in/api/unknown', {
+    method: 'GET'
+})
+.then( function(response) {
+    if(!response.ok) {
+        throw response.status
+    }
+    return response.json();
+})
+.then( function(responseData) {
+
+    const ulElement = document.createElement('ul');
+
+    responseData.data.forEach((element) => {
+        // console.log(element)
+        let li = document.createElement('li');
+        
+        // li.textContent = element.name + ' ' + element.color;
+        li.textContent = `${element.name} ${element.color}`;
+        ulElement.appendChild(li);
+    });
+    apiUsers.appendChild(ulElement)
+    // console.log(responseData)
+})
+.catch( function(error) {
+
+    if (error == 404) {
+        const pDesc = document.createElement('p');
+        pDesc.textContent = 'Page Not Found';
+        apiUsers.appendChild(pDesc);
+    }
+    // console.log(error)
+})
